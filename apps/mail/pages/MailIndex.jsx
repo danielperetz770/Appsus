7f3538e1711aa26d31b77d7f8c2c3bc17ad53a95
yandimant1993 <yan1993dimant@gmail.com>
2025-07-30 15:42:34 +0200

    Render Mails

--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,5 +1,38 @@
 
-export function MailIndex() {
-    return <section className="container">Mail app</section>
-}
+import { mailService } from "../services/mail.service.js";
+const { useState, useEffect } = React
 
+export function MailIndex() {
+
+    const [mails, setMails] = useState([])
+
+
+    useEffect(() => {
+        loadMails()
+    }, [])
+
+    function loadMails() {
+        mailService.query()
+            .then(mails => setMails(mails))
+            .catch(err => console.log('failed to load', err))
+    }
+    if (!mails || !mails.length) return <div>loading...</div>
+    return (
+        <React.Fragment>
+            <section className="container">Mail app
+                <ul className="mail-list">
+                    {mails.map(mail => (
+                        <li key={mail.id} className="mail">
+                            <p>subject:{mail.subject}</p>
+                            <p>{mail.body}</p>
+                            <p>{mail.isRead}</p>
+                            <p>from:{mail.from}</p>
+                            <p>time sent:{mail.sentAt}</p>
+                        </li>
+                    ))
+                    }
+                </ul>
+            </section>
+        </React.Fragment>
+    )
+}
