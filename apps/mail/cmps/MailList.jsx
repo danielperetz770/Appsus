f7d9181ab3a322f81a0ac13b351a6cb79fa27e48
yandimant1993 <yan1993dimant@gmail.com>
2025-07-30 20:53:50 +0200

    Create modular Cmps

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,3 +1,29 @@
-export function MailList() {
-    return <div>Mail list</div>
+
+import { MailPreview } from "../../mail/cmps/MailPreview.jsx";
+import { MailIndex } from "../pages/MailIndex";
+
+
+export function MailList({ mails, onSetSelectedMail, selectedMail }) {
+
+    return (
+        <React.Fragment>
+            <div className="header flex space-between align-center">
+                <div className="search-bar">
+                    <img className="google-pic" src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_2x_rtl_r5.png"></img>
+                </div>
+                <div className="picture">
+                    <input type="text" placeholder="Search in Gmail" />
+                </div>
+            </div>
+            <ul className="mail-list">
+                {mails.map(mail =>
+                    <MailPreview
+                        key={mail.id}
+                        mail={mail}
+                        onSetSelectedMail={onSetSelectedMail}
+                        selectedMail={selectedMail} />
+                )}
+            </ul >
+        </React.Fragment>
+    )
 }
