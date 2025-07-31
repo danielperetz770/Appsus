98981da3bdd0bfedfdb4788be79d3e65cb7289c4
Daniel Peretz <danielperetz620@gmail.com>
2025-07-31 18:50:49 +0300

    s messege

--- apps/note/cmps/ColorInput.jsx
+++ apps/note/cmps/ColorInput.jsx
@@ -20,7 +20,7 @@ export function ColorInput({ onSetColorStyle, backgroundColor }) {
                     title="No color"
                     onClick={() => onSetColor(null)}
                 >
-                   <img src="icon/keep-assets/asset 5.svg" alt="" />
+                    <img src="icon/keep-assets/asset 5.svg" alt="" />
                 </div>
                 {colors.map(color => (
                     <div
