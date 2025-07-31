70ff965b07edfc6999ea7bbfafcf587c2098623e
yandimant1993 <yan1993dimant@gmail.com>
2025-07-31 16:29:39 +0200

    ReadUnRead fueature

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -3,13 +3,18 @@
 const { useState, useEffect } = React
 
 
-export function MailPreview({ mail, onSetSelectedMail, selectedMail, DeleteMail}) {
+export function MailPreview({ mail, onSetSelectedMail, selectedMail, DeleteMail,setMails }) {
 
     function handleDeleteMail(ev) {
         ev.stopPropagation()
         DeleteMail(mail)
-
     }
+
+    function toggleReadUnRead(ev) {
+        ev.stopPropagation()
+        setMails(prevMails => prevMails.map(msg => msg.id === mail.id ? {...msg,isRead: !msg.isRead} : msg))
+
+}
 
     return (
         <React.Fragment>
@@ -18,6 +23,9 @@ export function MailPreview({ mail, onSetSelectedMail, selectedMail, DeleteMail}
                 onClick={() => onSetSelectedMail(mail)}>
                 <p>{mail.subject}</p>
                 <p>from:{mail.body}</p>
+                <button className="read" onClick={toggleReadUnRead}>
+                    {mail.isRead ? '📤' : '📩'}
+                    </button>
                 <button onClick={handleDeleteMail}>Delete</button>
             </li>
             <hr className="seperator"></hr>
